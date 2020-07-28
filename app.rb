6960c69c7025eae7ff2c77ed86a536897b6c9003
Sergei Chernov <dozzator@gmail.com>
2020-07-28 04:40:40 +0400

    some changes to database

--- app.rb
+++ app.rb
@@ -1,3 +1,10 @@
 require 'sqlite3'
 
-db = SQLite3::Database.new 'test.sqlite'
+db = SQLite3::Database.new 'NewTestDatabase.db'
+
+db.execute "SELECT * FROM Cars" do |car|
+	puts car
+	puts '==========='
+end
+
+db.close
